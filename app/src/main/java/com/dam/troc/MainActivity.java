700d401cf6cafa2246640a9c0ec6197bf29e0da5
Jacques <jacquesdongsimplon@gmail.com>
2022-11-25 11:12:50 +0100

    search

--- app/src/main/java/com/dam/troc/MainActivity.java
+++ app/src/main/java/com/dam/troc/MainActivity.java
@@ -5,7 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,9 +23,55 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity  {
 
+    ListView listView;
+    String[] name = {"Plombier","Developpeur","Mecanicien","Jardinier","Boulanger","Infirmier","Pilote","Armurier","Fermier","Moniteur","Ouvrier","Chauffeur","Skateur",};
+
+    ArrayAdapter<String> arrayAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_search);
+
+        listView = findViewById(R.id.lv_listview);
+
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        listView.setAdapter(arrayAdapter);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.search_menu,menu);
+
+        MenuItem menuItem = menu.findItem(R.id.search_menu);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Type here to search");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+
+                arrayAdapter.getFilter().filter(s);
+
+
+
+
+                return false;
+            }
+        });
+        
+
+
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
