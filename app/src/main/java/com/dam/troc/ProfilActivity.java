baa759898440a3913073ebde13c75c27f2f06c33
anthonyD <delfforge.anthony@gmail.com>
2022-12-01 09:13:01 +0100

    fix Classname

--- app/src/main/java/com/dam/troc/ProfilActivity.java
+++ app/src/main/java/com/dam/troc/ProfilActivity.java
@@ -17,6 +17,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
@@ -28,7 +29,7 @@ public class ProfilActivity extends Fragment {
     Button btnSubmit;
     ImageView profilimg;
     Uri imgUrl;
-    EditText plPseudo, plMel, plTel, plAdress, plVille, plCpostal, plComp1, plComp2,
+    TextView plPseudo, plMel, plTel, plAdress, plVille, plCpostal, plComp1, plComp2,
             plComp3, plDescription;
     FirebaseAuth mAuth;
     public static final int PICK_IMAGE = 1;
@@ -41,26 +42,26 @@ public class ProfilActivity extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         mAuth = FirebaseAuth.getInstance();
+      ViewGroup itemView = (ViewGroup)inflater.inflate(R.layout.activity_profil,container,false);
 
-
-        profilimg = profilimg.findViewById(R.id.iv_profile_photo);
-        plPseudo = plPseudo.findViewById(R.id.tv_user_profile_name);
+        profilimg = itemView.findViewById(R.id.iv_profile_photo);
+        plPseudo = itemView.findViewById(R.id.tv_user_profile_name);
         //plNom = findViewById(R.id.editTextTextProfilNom);
         //plPrenom = findViewById(R.id.EditTxtProfilPrenom);
-        plMel = plMel.findViewById(R.id.editTextProfilEmail);
-        plTel = plTel.findViewById(R.id.editTextTextProfilPhone);
-        plAdress = plAdress.findViewById(R.id.editTextProfilAdresse);
-        plVille = plVille.findViewById(editTextProfilVille);
-        plCpostal = plCpostal.findViewById(edittextProfilCpostal);
-        plComp1 = plComp1.findViewById(R.id.etprofilcomp1);
-        plComp2 = plComp2.findViewById(R.id.etprofilcomp2);
-        plComp3 = plComp3.findViewById(R.id.etprofilcomp3);
-        plDescription = plDescription.findViewById(R.id.et_profil_descriptionText);
+        plMel = itemView.findViewById(R.id.editTextProfilEmail);
+        plTel = itemView.findViewById(R.id.editTextTextProfilPhone);
+        plAdress = itemView.findViewById(R.id.editTextProfilAdresse);
+        plVille = itemView.findViewById(editTextProfilVille);
+        plCpostal = itemView.findViewById(edittextProfilCpostal);
+        plComp1 = itemView.findViewById(R.id.etprofilcomp1);
+        plComp2 = itemView.findViewById(R.id.etprofilcomp2);
+        plComp3 = itemView.findViewById(R.id.etprofilcomp3);
+        plDescription = itemView.findViewById(R.id.et_profil_descriptionText);
 
 
 
         loadUserInformation();
-        return (ViewGroup)inflater.inflate(R.layout.activity_profil,container,false);
+        return itemView;
 
     }
 
